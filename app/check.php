b4251dd57a856a2b9e85c4cfb696fdba94cdbbdb
Joan21Frances <110341093+Joan21Frances@users.noreply.github.com>
2022-07-31 22:43:11 +0800

    Add files via upload

---
+++ app/check.php
@@ -0,0 +1,32 @@
+<?php
+
+if(isset($_POST['id'])){
+    require '../db_conn.php';
+
+    $id = $_POST['id'];
+
+    if(empty($id)){
+       echo 'error';
+    }else {
+        $todo = $conn->prepare("SELECT id, checked FROM todo WHERE id=?");
+        $todo->execute([$id]);
+
+        $todo = $todo->fetch();
+        $uId = $todo['id'];
+        $checked = $todo['checked'];
+
+        $uChecked = $checked ? 0 : 1;
+
+        $res = $conn->query("UPDATE todo SET checked=$uChecked WHERE id=$uId");
+
+        if($res){
+            echo $checked;
+        }else {
+            echo "error";
+        }
+        $conn = null;
+        exit();
+    }
+}else {
+    header("Location: ../index.php?mess=error");
+}
